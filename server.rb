d087e224ea5be40d1e220f22643dfb915540f7db
Jay Deverett <jayjdeverett@gmail.com>
2017-07-12 15:58:53 -0400

    add favourites page

--- server.rb
+++ server.rb
@@ -1,9 +1,24 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   erb :index
 end
 
 get '/portfolio' do
   erb :gallery
+end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films']
+  erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['www.reddit.com/r/askreddit', 'www.reddit.com/r/outoftheloop', 'www.reddit.com/r/interestingasfuck', 'www.reddit.com/r/mildlyinteresting']
+  erb :favourites
 end
